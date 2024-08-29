5db692bb180388f2873d1ecdf4d53c9f42380fda
vox45 <golovkodave19@gmail.com>
2024-08-30 00:07:44 +0300

    first commit

---
+++ src/app/FundsException.java
@@ -0,0 +1,9 @@
+package src.app;
+
+// Кастомний виняток
+public class FundsException extends Exception {
+
+    public FundsException(String message) {
+        super(message);
+    }
+}
---
+++ src/app/Main.java
@@ -0,0 +1,43 @@
+package src.app;
+
+import java.util.Scanner;
+
+public class Main {
+
+    static double balance;
+
+    public static void main(String[] args) {
+        balance = getBalance();
+        validateAmount(balance, getAmount());
+    }
+
+    private static double getBalance() {
+        return 1000.00; // Наявні кошти на рахунку
+    }
+
+    private static double getAmount() {
+        System.out.printf("Balance is USD %.2f.%n" +
+                "Enter purchase amount, USD: ", balance);
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextDouble();
+    }
+
+    // Метод валідації наявних коштів
+    private static void validateAmount(double balance, double withdrawal) {
+        if (withdrawal > balance) {
+            try {
+                throw new FundsException("Insufficient funds!");
+            } catch (FundsException ex) {
+                System.out.println(ex.getMessage());
+            }
+        } else {
+            balance = getNewBalance(balance, withdrawal);
+            System.out.printf("Funds are OK. Purchase paid.%nBalance is USD %.2f", balance);
+        }
+    }
+
+    // Метод розрахунку наявних коштів на рахунку після зняття певної суми коштів
+    private static double getNewBalance(double balance, double withdrawal) {
+        return balance - withdrawal;
+    }
+}
